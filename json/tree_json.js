9a5c813f9ba5e55a0137b287eefe631e7c1816db
hiwylee <hiwylee@gmail.com>
2020-03-12 22:27:51 +0900

    Update tree_json.js

--- json/tree_json.js
+++ json/tree_json.js
@@ -1,1 +1,7 @@
 var json_data = 
+ [
+       { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" },
+       { "id" : "ajson2", "parent" : "#", "text" : "Root node 2" },
+       { "id" : "ajson3", "parent" : "ajson2", "text" : "Child 1" },
+       { "id" : "ajson4", "parent" : "ajson2", "text" : "Child 2" },
+ ];
