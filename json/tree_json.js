219701c99499d8d6516e218689a130e1c344cc55
hiwylee <hiwylee@gmail.com>
2020-03-12 22:43:58 +0900

    Update tree_json.js

--- json/tree_json.js
+++ json/tree_json.js
@@ -10,4 +10,19 @@ function getTreeJson ()
   ];    // code to be executed
    rerurn json_data;
 };
+    var tree_json2 = 
+    [
+          { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" },
+          { "id" : "ajson2", "parent" : "#", "text" : "Root node 2" },
+          { "id" : "ajson3", "parent" : "ajson2", "text" : "Child 1" },
+          { "id" : "ajson4", "parent" : "ajson2", "text" : "Child 2" },
+    ];
 
+    function getTreeJson()   
+    {
+        return tree_json2;
+    }
+
+function a(){
+ alert("aaa");
+}
